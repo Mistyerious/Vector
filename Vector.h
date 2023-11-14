ae105fcc74666211461e6a9a9155ecfb5d042d61
Mistyerious <dyeaaronjr@proton.me>
2023-11-13 17:15:42 -0800

    Modified insert to actually grow when capacity is reached

--- Vector.h
+++ Vector.h
@@ -5,6 +5,8 @@
 #ifndef VECTOR_VECTOR_H
 #define VECTOR_VECTOR_H
 
+#include <stdexcept>
+
 template <typename T>
 class Vector {
   T *array;
@@ -12,15 +14,46 @@ class Vector {
   size_t length;
 
  public:
-  Vector();
-  explicit Vector(size_t size);
+  Vector() : capacity(1), length(0), array(new int[1]) {};
+  explicit Vector(size_t size) : capacity(size), length(1), array(new int[size]) {};
 
-  constexpr size_t size() { return length; }
+  size_t size() const { return length; }
 
   inline void push_back(T element) { insert(length, element); }
-  void insert(size_t index, T element);
-  T &operator[](size_t index) const;
-  ~Vector();
+  void insert(size_t index, T element) {
+	  if (index >= length) {
+		  if (index >= capacity) {
+
+			  int oldCapacity = capacity;
+
+			  while (capacity < length) {
+				  capacity *= 2;
+			  }
+
+			  T *old = new T[capacity];
+
+			  memcpy(old, array, oldCapacity * sizeof(T));
+
+			  delete[] array;
+
+			  array = old;
+		  }
+		  length = index + 1;
+	  } else {
+		  length++;
+	  }
+
+	  array[index] = element;
+  }
+  T &operator[](size_t index) const {
+	if (index >= length) {
+		throw std::range_error("index out of range");
+	}
+	return array[index];
+  }
+  ~Vector() {
+	  delete[] array;
+  }
 };
 
 #endif  // VECTOR_VECTOR_H
--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ int main() {
   vec.insert(0, 100);
   vec.insert(1, 1000);
 
+//  vec.push_back(10000);
+
   std::cout << vec.size() << std::endl;
 
   for (int i = 0; i < vec.size(); i++) {
