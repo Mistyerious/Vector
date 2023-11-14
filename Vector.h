e3afd7e7565f8b4c29b104b5659a1f5a615b2dd4
Jwaffled <82175061+Jwaffled@users.noreply.github.com>
2023-11-13 19:25:17 -0600

    Fix heap corruption

--- Vector.h
+++ Vector.h
@@ -13,47 +13,49 @@ class Vector {
   size_t capacity;
   size_t length;
 
+  void resize_to_fit(size_t index) {
+    int newCapacity = capacity;
+    while (newCapacity <= index) {
+      newCapacity *= 2;
+    }
+
+    T* temp = new T[newCapacity];
+
+    memcpy(temp, array, sizeof(T) * capacity);
+    capacity = newCapacity;
+    delete[] array;
+    array = temp;
+  }
+
  public:
-  Vector() : capacity(1), length(0), array(new int[1]) {};
-  explicit Vector(size_t size) : capacity(size), length(1), array(new int[size]) {};
+  Vector() : capacity(1), length(0), array(new int[1]){};
+  explicit Vector(size_t size)
+      : capacity(size), length(1), array(new int[size]){};
 
   size_t size() const { return length; }
 
   inline void push_back(T element) { insert(length, element); }
+
   void insert(size_t index, T element) {
-	  if (index >= length) {
-		  if (index >= capacity) {
+    if (index >= capacity) {
+      resize_to_fit(index);
+    }
 
-			  int oldCapacity = capacity;
+    if (index >= length) {
+      length = index + 1;
+    } else {
+      length++;
+    }
 
-			  while (capacity < length) {
-				  capacity *= 2;
-			  }
-
-			  T *old = new T[capacity];
-
-			  memcpy(old, array, oldCapacity * sizeof(T));
-
-			  delete[] array;
-
-			  array = old;
-		  }
-		  length = index + 1;
-	  } else {
-		  length++;
-	  }
-
-	  array[index] = element;
+    array[index] = element;
   }
   T &operator[](size_t index) const {
-	if (index >= length) {
-		throw std::range_error("index out of range");
-	}
-	return array[index];
+    if (index >= length) {
+      throw std::range_error("index out of range");
+    }
+    return array[index];
   }
-  ~Vector() {
-	  delete[] array;
-  }
+  ~Vector() { delete[] array; }
 };
 
 #endif  // VECTOR_VECTOR_H
